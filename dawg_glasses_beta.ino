b1db99e27a6bf2f0e2a90f6a8dd5be26b56eef39
Akhil Chandran <zxaryo@gmail.com>
2016-08-30 08:20:41 +0530

    Beta update

---
+++ dawg_glasses_beta.ino
@@ -0,0 +1,19 @@
+#include<IRremote.h>
+IRsend irsend;
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(13,OUTPUT)
+  digitalWrite(13,1);
+}
+void loop() 
+{
+  if (Serial.read() !=-1)
+  {
+   for (int i=0; i<3; i++)
+    {
+      irsend.sendNEC(0x00ff01fe, 32);
+      delay(400);
+    }
+  }
+}

07fe36879880dad84aef1d4cf41c5289d37a48ba
Akhil Chandran <zxaryo@gmail.com>
2016-08-31 19:21:21 +0530

    Test code, getting data

---
+++ irreciver.ino
@@ -0,0 +1,24 @@
+#include <IRremote.h>
+
+int RECV_PIN = 4;
+
+IRrecv irrecv(RECV_PIN);
+long int x;
+decode_results results;
+
+void setup()
+{
+  Serial.begin(9600);
+  irrecv.enableIRIn(); // Start the receiver
+}
+
+void loop()
+{
+  if (irrecv.decode(&results)) 
+  {
+    Serial.println(results.value, HEX);
+    x=results.value;
+    Serial.println(x);
+    irrecv.resume(); // Receive the next value
+  }
+}

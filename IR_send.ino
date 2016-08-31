48bc3c1fa0d95e5b37749c5c35e3505e4cfaa688
Akhil Chandran <zxaryo@gmail.com>
2016-08-31 19:56:17 +0530

    Add files via upload

---
+++ IR_send.ino
@@ -0,0 +1,17 @@
+#include<IRremote.h>
+IRsend irsend;
+void setup() 
+{
+  Serial.begin(9600);
+}
+void loop() 
+{
+  if (Serial.read() !=-1)
+  {
+   //for (int i=0; i<3; i++)
+    {
+      irsend.sendNEC(0x00ff01fe, 32);
+      delay(400);
+    }
+  }
+}

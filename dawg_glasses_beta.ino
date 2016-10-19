c4b9229534e151486477b51247983647c91c9165
Akhil Chandran <zxaryo@gmail.com>
2016-10-20 00:34:09 +0530

    Update dawg_glasses_beta.ino

--- dawg_glasses_beta.ino
+++ dawg_glasses_beta.ino
@@ -1,15 +1,13 @@
-#define led_red 3
-#define led_org 2
 #define d0 5
 #define d1 6
 #define d2 7
 #define d3 8
 #define d4 9
+#define led_red 3
+#define led_org 2
 #define motor_a 12
 #define motor_b 13
-
 #include<IRremote.h>
-
 int  spp=40, mdel;
 byte a[10][4]={
    {0,0,0,0}
@@ -22,7 +20,6 @@ byte a[10][4]={
 ,  {0,1,1,1}
 ,  {1,0,0,0}
 ,  {1,0,0,1}};
-
 int RECV_PIN = 4;
 IRrecv irrecv(RECV_PIN);
 decode_results results;
@@ -40,21 +37,22 @@ void setup()
  pinMode(motor_b,OUTPUT);
  Serial.begin(9600);
  irrecv.enableIRIn();
- //setTime(0);
 }
 void indicator()
-{ for(int i=0;i<5;i++)
- {
-  digitalWrite(led_org,1);
+{
+  for(int i=0;i<5;i++)
+   {
+      digitalWrite(led_org,1);
+      delay(500);
+      digitalWrite(led_org,0);
+      delay(500);
+   }
+  digitalWrite(led_red,1);
   delay(500);
-  digitalWrite(led_org,0);
-  delay(500);
- }
-   digitalWrite(led_red,1);
-   delay(500);
 }
 void ds(int sped)
-{   if(sped>0)
+{  
+    if(sped>0)
     digitalWrite(motor_a,1);
     digitalWrite(motor_b,0);
     int x=sped%10;
@@ -64,11 +62,11 @@ void ds(int sped)
     delay(4);
     digitalWrite(d0,1);
     dd(y);
-    delay(4); 
+    delay(4);   
     digitalWrite(motor_a,0);
-    if(sped==60) mdel=4;
-    else if(sped==40) mdel==6;
-    else if(sped==20) mdel==8;
+    if(sped==60) mdel=0;
+    else if(sped==40) mdel==100;
+    else if(sped==20) mdel==200;
     delay(mdel);
 }
 int cmap(char zx)
@@ -91,7 +89,6 @@ void dd(int x)
   digitalWrite(d2,a[x][2]);
   digitalWrite(d3,a[x][1]);
   digitalWrite(d4,a[x][0]);
-
 }
 void loop()
 { 
@@ -102,7 +99,7 @@ void loop()
     else if (results.value==0xa92)  spp=40;
     else if (results.value==0xa93)  spp=20;
     else if (results.value==0xa95)  spp=0;
-    Serial.print(spp);
+    Serial.println(spp);
     irrecv.resume(); 
   }
     ds(spp);

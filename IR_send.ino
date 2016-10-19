0e6ab486d22697470df63f66fc45c8c50c465756
Akhil Chandran <zxaryo@gmail.com>
2016-10-19 22:02:41 +0530

    Update IR_send.ino

--- IR_send.ino
+++ IR_send.ino
@@ -1,3 +1,6 @@
+#define ir_led 3
+#define led 13
+#define buz 9
 #include <Time.h>
 #include <TimeLib.h>
 #include<IRremote.h>
@@ -7,76 +10,80 @@ int val=50, bks=0, k=0;
 byte ey=1,iy=0;
 void setup() 
 {
-  pinMode(3,OUTPUT);  //IR_LED
-  pinMode(9,OUTPUT);  //BUZZER
-  pinMode(13,OUTPUT); //LED
+  pinMode(ir_led,OUTPUT);
+  pinMode(buz,OUTPUT);
+  pinMode(led,OUTPUT);
   Serial.begin(9600);
-  
-{  digitalWrite(13,1);
-  digitalWrite(9,1);
-  delay(100);
-  digitalWrite(13,0);
-  digitalWrite(9,0);
-  delay(75);
-  digitalWrite(13,1);
-  digitalWrite(9,0);
-  delay(250);
-  digitalWrite(9,0);
-  digitalWrite(13,0);
+
+  ind(led,100,2);
+  ind(buz,100,2);
   irsend.sendSony(0xa92, 12);
 }
+void ind(int pin,int del,int tim)
+{
+  for(int i=0;i<tim;i++)
+  {
+    digitalWrite(pin,1);
+    delay(del);
+    digitalWrite(pin,0);
+    delay(del);
+  }
 }
 void cal(int bk)
 {
-  digitalWrite(13,1);
-  for(int i=0; i<3; i++)
-  {
-    if(bk<4)      irsend.sendSony(0xa91, 12);
-    else if(bk<7) irsend.sendSony(0xa92, 12);
-    else if(bk<13) irsend.sendSony(0xa93, 12);
-  }
-  delay(50);
-  digitalWrite(13,0);
+    for(int i=0; i<3; i++)
+    {
+        if(bk<4)   
+        {
+          irsend.sendSony(0xa91, 12);
+          ind(led,50,3);
+        }
+        else if(bk<7) 
+        {
+          irsend.sendSony(0xa92, 12);
+          ind(led,50,2);
+        }
+        else if(bk<led) 
+        {
+          irsend.sendSony(0xa93, 12);
+          ind(led,50,3);
+        }
+    }
 }
 void loop() 
 {
   delay(1000);
   while(second()%15!=0)
-  { 
-     if(analogRead(A5)<val)
-      ey=0;
-     else ey=1;
-     
-     if(ey!=iy) 
-     {
-        bks++;
-        Serial.println(bks);
-     }
-     iy=ey;
-  }
-    if(bks==0&&k==1)
-  {
-    for(int j=0; j<500; j++)
-    {
-     irsend.sendSony(0xa95, 12);
-     digitalWrite(9,1);
-     digitalWrite(13,0);
-     delay(1000);
-     digitalWrite(9,0);
-     digitalWrite(13,0); 
-     delay(1000);
-    }
-  }
-  if(bks==0&&k==0)
-  {
-    k=1;
-    digitalWrite(9,1);
-    digitalWrite(13,1);
-    delay(3000);
-    digitalWrite(9,0);
-    digitalWrite(13,0);
-  }
-
+      { 
+         if(analogRead(A5)<val)
+          ey=0;
+         else ey=1;
+         
+         if(ey!=iy) 
+         {
+            bks++;
+            Serial.println(bks);
+         }
+         iy=ey;
+      }
+  if(bks>=14) bks=0;
+  if(bks==0&&k==1)
+      {
+        for(int j=0; j<500; j++)
+        {
+         irsend.sendSony(0xa95, 12);
+         ind(led,50,1);
+         ind(buz,500,10);
+        }
+      }
+      if(bks==0&&k==0)
+      {
+        k=1;
+        ind(led,100,1);
+        ind(buz,500,2);
+        ind(led,100,1);
+        ind(buz,500,1);
+      }
   bks/=2;
   cal(bks);
   bks=0;
